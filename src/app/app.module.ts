c8ec16d240123e295bf1edf132185e8536e20c6a
AlexVlasuk <alexandervlasyk@gmail.com>
2019-06-11 17:33:06 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MainComponent } from './main/main.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'contact', component: ContactComponent },
+ // { path: 'hero/:id', component: HeroDetailComponent },
+  {
+    path: 'main',
+    component: MainComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    MainComponent,
+    AboutUsComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
